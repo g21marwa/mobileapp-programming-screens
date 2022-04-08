ad991e236b541d12deb44581c85f35dff4cc57df
g21marwa <g21marwa@student.his.se>
2022-04-08 16:51:33 +0200

    added a way to get intent and disply messages sent from main activity.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String message = "inputText";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/screens/SecondaryActivity.java
+++ app/src/main/java/com/example/screens/SecondaryActivity.java
@@ -1,13 +1,21 @@
 package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondaryActivity extends AppCompatActivity {
+    TextView recieve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+        recieve = (TextView) findViewById(R.id.recieveText);
+        Intent getIntent = getIntent();
+        String getMessage = getIntent.getStringExtra(MainActivity.message);
+        recieve.setText(getMessage);
     }
 }
