c62be38fd1a404aeec96cbe9b3067ae3af602c9b
g21marwa <g21marwa@student.his.se>
2022-04-08 16:05:05 +0200

    Added a secondary activity and a button to open it.

---
+++ app/src/main/java/com/example/screens/SecondaryActivity.java
@@ -0,0 +1,13 @@
+package com.example.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+    }
+}
